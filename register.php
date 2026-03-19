b06b3034d25df295bfe6432cda270b633657c5ad
timcrepinpro <tim.crepin@edouardgand.fr>
2026-03-19 16:48:07 +0100

    register1.0

---
+++ register.php
@@ -0,0 +1,52 @@
+<?php
+
+
+$login = $_POST['login'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
+echo "Login: $login, Email: $email, Password: $password, Confirm Password: $confirm_password";
+
+
+
+if (preg_match($pattern, $password)) {
+echo "Mot de passe valide";
+} else {
+echo "Mot de passe invalide";
+exit
+}
+
+if ($_post['password']==$_post['confirm_password']) {
+
+
+}
+/*
+
+
+
+session_start();
+$login = $_POST['identifier'];
+$password = $_POST['password'];
+if (!empty($login) && !empty($password)) {
+$_SESSION['login'] = $login;
+header("Location: index.php");
+exit;
+} else {
+echo "Erreur : champs manquants";
+}
+
+
+if ($_post['password']==$_post['confirm_password']) {
+    $login = $_post['login'];
+    $email = $_post['email'];
+    $password = $_post['password'];
+    $confirm_password = $_post['confirm_password'];
+    echo "";
+} else {
+    echo "";
+}
+
+
+
+*/
+?>
